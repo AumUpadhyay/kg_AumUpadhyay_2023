64abb533ab862ee3a6589db73c0d852ffe7de453
AumUpadhyay <aumspeaks@gmail.com>
2020-03-13 22:42:50 -0400

    solution code commit - adds mapping and checks existing mapping

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * @author Aum Upadhyay
  *
@@ -6,8 +8,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		String s1 = "abc";
-		String s2 = "xyz";
+		// TODO Auto-generated method stub
+		String s1 = "aac";
+		String s2 = "xxz";
 		System.out.println(isOneToOne(s1, s2));
 
 	}
@@ -28,6 +31,25 @@ public class Main {
 			return false;
 		}
 
+		int[] charMap = new int[256];
+		Arrays.fill(charMap, -1); //-1 is default since it is not used in ascii code
+
+		for (int stringIndex = 0; stringIndex < s1.length(); stringIndex++) {
+
+			char s1Current = s1.charAt(stringIndex);
+			char s2Current = s2.charAt(stringIndex);
+
+			if (charMap[s1Current] == -1) {
+				if (charMap[s2Current] == -1) {
+					//if neither s1 character or s2 character has mapping, a new mapping formed between them
+					charMap[s2Current] = s1Current;
+					charMap[s1Current] = s2Current;
+				} else {
+					//if s2 character has mapping, but s1 character doesn't strings cannot be one-to-one
+					return false;
+				}
+			}
+		}
 		return true;
 	}
 
